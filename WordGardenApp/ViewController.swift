f9c60b3f55b5ed16ff05c0ab1ec869e2fff8ac60
Will Templeton <templetw@bc.edu>
2020-02-02 13:09:30 -0500

    commit 3.1  - user interface

--- WordGardenApp/ViewController.swift
+++ WordGardenApp/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var guessCountLabel: UILabel!
+    @IBOutlet weak var flowerImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
